cac74cf261f595002c188b7fc110fa32955aa5b4
mohammeds <mohammeds@somnoware.com>
2018-12-27 17:28:52 +0530

    es6-classes

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -11,8 +11,25 @@ class Person {
   }
 }
 
-const me = new Person('Andrew Mead', 26);
+class Student extends Person { 
+  constructor(name, age, major) {
+    super(name,age);
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description =  super.getDescription();
+    if (this.hasMajor()) {
+      description += `Their major is ${this.major}`
+    }
+    return description;
+  }
+}
+
+const me = new Student('Andrew Mead', 26,'Computer Science');
 console.log(me.getDescription());
 
-const other = new Person();
+const other = new Student();
 console.log(other.getDescription());
